9152ea22a9dd6e4cfd834913d46127efda86cfe6
zlic <MF20320220@smail.nju.edu.cn>
2020-11-19 20:06:57 +0800

    ✨feat(评分可视化): 添加后端接口api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,9 @@
 import { instance } from "../util/request";
 import {
-    HOT_TODAY_DATA
+    HOT_TODAY_DATA,
+    CARTOON_INFO_ARR,
+    CARTOON_RANK_PATH,
+    TIME_RANGE_CARTOON_RANK_PATH
 } from "../consts/server";
 
 async function getHotTodayData () {
@@ -9,6 +12,38 @@ async function getHotTodayData () {
     return result;
 }
 
+async function getCartoonInfoArr () {
+    const response = await instance.get(CARTOON_INFO_ARR);
+    const result = response.data;
+    return result;
+}
+
+async function getCartoonRankPath (id) {
+    const requestConfig = {
+        params: {
+            id
+        }
+    };
+    const response = await instance.get(CARTOON_RANK_PATH, requestConfig);
+    const result = response.data;
+    return result;
+}
+
+async function getTimeRangeCartoonRankPath (from, to) {
+    const requestConfig = {
+        params: {
+            from,
+            to
+        }
+    };
+    const response = await instance.get(TIME_RANGE_CARTOON_RANK_PATH, requestConfig);
+    const result = response.data;
+    return result;
+}
+
 export {
-    getHotTodayData
+    getHotTodayData,
+    getCartoonInfoArr,
+    getCartoonRankPath,
+    getTimeRangeCartoonRankPath
 };
--- src/consts/server.js
+++ src/consts/server.js
@@ -3,9 +3,17 @@
 // TODO 需要修改
 const serverHost = "localhost";
 const serverPort = 8888;
+
 const HOT_TODAY_DATA = "/hotTodayData";
 
+const CARTOON_INFO_ARR = '/cartoonInfoArr';
+const CARTOON_RANK_PATH = '/cartoonRankPath';
+const TIME_RANGE_CARTOON_RANK_PATH = '/timeRangeCartoonRankPath';
+
 export {
+    CARTOON_INFO_ARR,
+    CARTOON_RANK_PATH,
+    TIME_RANGE_CARTOON_RANK_PATH,
     HOT_TODAY_DATA,
     serverHost,
     serverPort
